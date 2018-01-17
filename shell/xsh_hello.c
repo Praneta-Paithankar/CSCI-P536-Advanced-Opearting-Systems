33f24a0afc849338e67f123a5722599da6837f87
Praneta Vishwanath Paithankar <ppaithan@silo.soic.indiana.edu>
2018-01-17 17:12:49 -0500

    Comments added.

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -24,13 +24,14 @@ shellcmd xsh_hello(int nargs, char *args[]) {
         }
 
         /* Check argument count */
-
+	/*Number of arguments are greater than 2,print the error*/
         if (nargs > 2) {
                 fprintf(stderr, "%s: too many arguments\n", args[0]);
                 fprintf(stderr, "Try '%s --help' for more information\n",
                         args[0]);
                 return 1;
         }
+	/*Number of arguments are less than 2.Print the error */
 	if(nargs <2){
 		fprintf(stderr, "%s: too few arguments\n", args[0]);
                 fprintf(stderr, "Try '%s --help' for more information\n",
@@ -38,6 +39,7 @@ shellcmd xsh_hello(int nargs, char *args[]) {
                 return 1;
 
 	}
+	/*Print the message.*/
         if (nargs == 2) {
                printf("Hello %s, Welcome to the world of Xinu!!",args[1]);
         }
