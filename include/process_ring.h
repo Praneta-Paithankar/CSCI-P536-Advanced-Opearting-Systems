7ccf0588fb4483aab1c3cc54cbd5661126b16cfd
ppaithan <ppaithan@iu.edu>
2018-01-22 10:56:42 -0500

    Update process_ring.h

--- include/process_ring.h
+++ include/process_ring.h
@@ -1,4 +1,5 @@
 /* process_ring.h*/
 
 extern process polling(int32* currentProcess,int32* nextProcess,int32 currentRing,int32 currentRound);
+extern process polling2(int32* currentProcess,int32* nextProcess,int32 roundCount,int32 currentRing);
 extern process message_passing(pid32 mainPid,pid32* nextProcessPid,int32 roundCount,int32 currentRing);
